836687fda8d8fc09112daa46d3be103a1a7d31c5
MatofSteel1 <matthewjameshull123@gmail.com>
2015-01-28 16:36:48 -0800

    Umm I'm not quite sure what you mean?

--- RecycleRush2015/src/org/usfirst/frc/team3674/robot/subsystems/LiftMechanism.java
+++ RecycleRush2015/src/org/usfirst/frc/team3674/robot/subsystems/LiftMechanism.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team3674.robot.subsystems;
 
+import org.usfirst.frc.team3674.robot.OI;
 import org.usfirst.frc.team3674.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,8 +13,14 @@ public class LiftMechanism extends Subsystem {
 	
 	public LiftMechanism() {
 		liftTalon = new Talon(RobotMap.liftTalonPort);
-		liftTalon.set(0.0);	
+		liftTalon.set(0.0);
+		if(OI.stick2.getRawAxis(3) >= 0.0) {
+			
+		} else if(OI.stick2.getRawAxis(3) <= 0.0) {
+			
+		}
 	}
+	
 		
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
