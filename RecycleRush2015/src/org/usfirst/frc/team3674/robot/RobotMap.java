60ba78eded8f4a208ec0efaf15d4be3b50108dd0
MatofSteel1 <matthewjameshull123@gmail.com>
2015-01-28 22:29:36 -0800

    Functions!!!

--- RecycleRush2015/src/org/usfirst/frc/team3674/robot/RobotMap.java
+++ RecycleRush2015/src/org/usfirst/frc/team3674/robot/RobotMap.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3674.robot;
 public class RobotMap {
 	
 	public static int frontLeftMotor = 1, rearLeftMotor = 2,
-			frontRightMotor = 3, rearRightMotor = 4, liftTalonPort = 1;
+			frontRightMotor = 3, rearRightMotor = 4, liftTalonPort = 1,
+			encoderPort1 = 1, encoderPort2 = 2;
 	
 }
--- RecycleRush2015/src/org/usfirst/frc/team3674/robot/subsystems/LiftMechanism.java
+++ RecycleRush2015/src/org/usfirst/frc/team3674/robot/subsystems/LiftMechanism.java
@@ -1,27 +1,37 @@
 package org.usfirst.frc.team3674.robot.subsystems;
 
 import org.usfirst.frc.team3674.robot.RobotMap;
-import org.usfirst.frc.team3674.robot.commands.LiftFromInput;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class LiftMechanism extends Subsystem {
 	
 	private Talon liftTalon;
+	private Encoder liftEncoder;
+	public double speedIncrease = 0;
 	
 	public LiftMechanism() {
 		liftTalon = new Talon(RobotMap.liftTalonPort);
+    	liftEncoder = new Encoder(RobotMap.encoderPort1, RobotMap.encoderPort2);
 		liftTalon.set(0.0);
 	}
 
     public void initDefaultCommand() {
-        setDefaultCommand(new LiftFromInput());
     }
     
     public void setSpeed(double speed) {
     	liftTalon.set(speed);
     }
     
+    public void liftSpeed(){
+    	if(liftEncoder.getStopped() == true) {
+    		speedIncrease++;
+    		this.setSpeed(speedIncrease);
+    		this.liftSpeed();
+    	} else {
+    		//speedIncrease = new stoppingSpeed;
+    	}
+    }
 }
-
