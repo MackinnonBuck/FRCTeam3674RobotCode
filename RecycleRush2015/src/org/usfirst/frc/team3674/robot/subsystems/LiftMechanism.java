34bd9da1d46c967eaf786e797691446fc5e80d61
MatofSteel1 <matthewjameshull123@gmail.com>
2015-01-30 19:12:43 -0800

    Function!!!

--- RecycleRush2015/src/org/usfirst/frc/team3674/robot/subsystems/LiftMechanism.java
+++ RecycleRush2015/src/org/usfirst/frc/team3674/robot/subsystems/LiftMechanism.java
@@ -11,28 +11,19 @@ public class LiftMechanism extends Subsystem {
 	
 	private Talon liftTalon;
 	private Encoder liftEncoder;
-	public double speedIncrease = 0;
+	private int targetSpeed;
 	
 	public LiftMechanism() {
 		liftTalon = new Talon(RobotMap.liftTalonPort);
     	liftEncoder = new Encoder(RobotMap.encoderPort1, RobotMap.encoderPort2);
 		liftTalon.set(0.0);
+		targetSpeed = 0;
 	}
 
     public void initDefaultCommand() {
     	setDefaultCommand(new MaintainClipPosition());
     }
-    
-    public void liftSpeed(){
-    	if(liftEncoder.getStopped() == true) {
-    		speedIncrease++;
-    		this.setPower(speedIncrease);
-    		this.liftSpeed();
-    	} else {
-    		//speedIncrease = new stoppingSpeed;
-    	}
-    }
-    
+        
     public void setPower(double power) {
     	liftTalon.set(power);
     }
@@ -42,32 +33,24 @@ public class LiftMechanism extends Subsystem {
     }
     
     public double getSpeed() {
-    	// Insert code here. Any existing code below is a placeholder.
-    	return 0.0;
+    	return liftEncoder.getRate();
     }
     
     public double getTargetSpeed() {
-    	// Insert code here. Any existing code below is a placeholder.
-    	return 0.0;
+    	return targetSpeed;
     }
     
-    public double getRateOfAcceleration() {
-    	// Insert code here. Any existing code below is a placeholder.
-    	return 0.0;
-    }
-    
-    public void setTargetPosition(int targetPosition) {
+    /*public void setTargetPosition(int targetPosition) {
     	// Insert code here. Any existing code below is a placeholder.
     }
     
     public int getStartingPosition() {
     	// Insert code here. Any existing code below is a placeholder.
     	return 0;
-    }
+    }*/
     
     public int getAngle() {
-    	// Insert code here. Any existing code below is a placeholder.
-    	return 0;
+    	return liftEncoder.getRaw();
     }
     
 }
