f88e13e01b0a470b67adbbc008d468caf04385b7
MatofSteel1 <matthewjameshull123@gmail.com>
2015-02-17 22:02:29 -0800

    basic autonomous (untested).

--- RecycleRush2015/src/org/usfirst/frc/team3674/robot/commands/AutonomousCommand.java
+++ RecycleRush2015/src/org/usfirst/frc/team3674/robot/commands/AutonomousCommand.java
@@ -8,7 +8,12 @@ public class AutonomousCommand extends CommandGroup {
     
 	public  AutonomousCommand() {
 		if(SmartDashboard.getString("Autonomous Mode:") == "1"){
-			
+			addSequential(new BasicDriveFromPresets(0, -0.5, 0, 2));
+			addSequential(new BasicDriveFromPresets(0, 0, -0.5, 1));
+			addSequential(new BasicDriveFromPresets(0, -1, 0, 0.5));
+			addSequential(new BasicDriveFromPresets(-0.25, 0, 0, 0.25));
+			addSequential(new return 0); //lift totes
+			addSequential(new BasicDriveFromPresets(0.25, 0, 0, 0.25));
 		}
 		if(SmartDashboard.getString("Autonomous Mode:") == "2"){
 			
@@ -18,4 +23,3 @@ public class AutonomousCommand extends CommandGroup {
 		}
 	}
 }
-
